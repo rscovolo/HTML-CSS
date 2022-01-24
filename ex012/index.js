0bd036bb232fb685da3bb2b9159d3d489f066789
rscovolo <ricardo.covolo@gmail.com>
2022-01-24 15:28:02 -0300

    Initial commit

---
+++ ex012/index.js
@@ -0,0 +1,14 @@
+
+    document.getElementById("meuiframe").setAttribute("src", "https://www.youtube.com/embed/3hng-hmSv2Y");
+
+
+
+
+
+    
+
+    var minhavariavel;
+
+    minhavariavel = "Ricardo";
+
+    document.getElementById("meutitulo").innerHTML = minhavariavel;
